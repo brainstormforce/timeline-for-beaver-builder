16fe653e0f5b5c8641603fb60fc496529099b318
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-03-15 18:50:39 +0530

    phpcs

--- timeline-for-beaver-builder-module/includes/frontend.js.php
+++ timeline-for-beaver-builder-module/includes/frontend.js.php
@@ -8,25 +8,25 @@
 ?>
 
 (function($) {
-	
+
 	$(document).ready(function() {
-	
+
 		<?php
 		if ( 'no' != $settings->tm_animation ) {
 			foreach ( $settings->timeline1 as $i => $item ) {
 				?>
 				new BBTimelineAnimation({
-					id: '<?php echo $id; ?>',
-					timeline: <?php echo $i; ?>,
-					mobile_screen: '<?php echo $settings->anim_mobile_on_off; ?>',
-					animation_delay: '<?php echo ( '' != $settings->tm_animation_delay && '0' != $settings->tm_animation_delay ) ? $settings->tm_animation_delay : ''; ?>',
-					animation: '<?php echo ( 'no' == $settings->tm_animation ) ? 'no' : 'animated ' . $settings->tm_animation; ?>',
-					viewport_position: '<?php echo ( '' != $settings->tm_viewport_position ) ? $settings->tm_viewport_position : '90'; ?>'
+					id: '<?php echo esc_attr( $id ); ?>',
+					timeline: <?php echo esc_attr( $i ); ?>,
+					mobile_screen: '<?php echo esc_attr( $settings->anim_mobile_on_off ); ?>',
+					animation_delay: '<?php echo esc_attr( ( '' != $settings->tm_animation_delay && '0' != $settings->tm_animation_delay ) ? $settings->tm_animation_delay : '' ); ?>',
+					animation: '<?php echo esc_attr( ( 'no' == $settings->tm_animation ) ? 'no' : 'animated ' . $settings->tm_animation ); ?>',
+					viewport_position: '<?php echo esc_attr( ( '' != $settings->tm_viewport_position ) ? $settings->tm_viewport_position : '90' ); ?>'
 				});
 				<?php
 			}
 		}
 		?>
 	});
-	
+
 })(jQuery);
